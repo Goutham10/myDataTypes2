11454a2c78e5f6fd2555b533fb193846ce38f58f
Goutham10 <boine.goutham10@gmail.com>
2018-09-11 17:36:52 +0530

    updated with constructors in employee class

--- employee/Employee.java
+++ employee/Employee.java
@@ -1,27 +1,54 @@
 package myDataTypes.employee;
 import myDataTypes.*;
-public class Employee {
 
-	public static void workPerfomance()
+public class Employee 
+{
+	String empAge;
+	String empName;
+	String empID;
+	String ephoneNumber;
+	String eaddress;
+	String empEmail;
+	
+	public void workPerfomance()
 	{
-		
+		System.out.println(" good ");
 	}
-	public static void remarks()
+	public void remarks()
 	{
-		
+		System.out.println(" not bad ");
 	}
-	public static void currentStatus()
+	public void currentStatus()
 	{
-		
+		System.out.println(" working ");
+	}
+	public Employee()
+	{
+		System.out.println(" default construct ");
+
+	}
+	public Employee(String age,String eName,String eID,String phoneNumber,String address,String email)
+	{
+		empAge=age;
+		empName=eName;
+		empID=eID;
+		ephoneNumber=phoneNumber;
+		eaddress=address;
+		empEmail=email;
+		System.out.println(" parameterised ");
+
 	}
 	public static void main(String[] args) 
 	{
-		byte age;
-		char eName;
-		int eID;
-		long phoneNumber;
-		char address;
-		char email;
+		
+		Employee e=new Employee();
+		e.workPerfomance();
+		e.remarks();
+		e.currentStatus();
+		Employee e1=new Employee("1","goutham","34","86593364","1-9-1015","goutham@gmail.com");
+		e1.workPerfomance();
+		e1.remarks();
+		e1.currentStatus();
 	}
 
 }
--- entertainment/IPLTicket.java
+++ entertainment/IPLTicket.java
@@ -1,15 +1,23 @@
 package myDataTypes.entertainment;
 import myDataTypes.*;
 
-public class IPLTicket {
-
-	public static void innings()
+public class IPLTicket 
+{
+	public void innings()
 	{
-		
+		System.out.println(" game going on.....");
 	}
-	public static void breakTime()
+	public void breakTime()
 	{
-		
+		System.out.println(" break time ");
+	}
+	public IPLTicket()
+	{
+		System.out.println(" default construct ");
+	}
+	public IPLTicket(char [] ticketNumber,double cost,char typeOfTicket,char stadiumName,int date)
+	{
+		System.out.println(" parameterised ");
 	}
 	public static void main(String[] args) 
 	{
@@ -18,7 +26,9 @@ public class IPLTicket {
 		char typeOfTicket;
 		char stadiumName;
 		int date;
-		
+		IPLTicket i= new IPLTicket();
+		i.innings();
+		i.breakTime();
 	}
 
 }
